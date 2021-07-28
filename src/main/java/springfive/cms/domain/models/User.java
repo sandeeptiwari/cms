20d2db185d4d94a8cbc631a80c9accd738f50074
Sandeep Tiwari <sandeep15mca@gmail.com>
2021-07-28 21:50:20 +0530

    feature(mongodb): add mongo support

---
+++ src/main/java/springfive/cms/domain/models/User.java
@@ -0,0 +1,29 @@
+package springfive.cms.domain.models;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ * @author claudioed on 28/10/17. Project cms
+ */
+@Data
+@Document(collection = "users")
+public class User {
+
+  @Id
+  String id;
+
+  String identity;
+
+  String name;
+
+  Role role;
+
+  public User(String id) {
+    this.id = id;
+  }
+
+  public User() {}
+
+}
